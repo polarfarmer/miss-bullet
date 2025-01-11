c38db3d99075fcf21e9f5f77c962279c6b273822
polarfarmer <a01040798163@gmail.com>
2025-01-11 17:58:05 +0900

    Grid_size

--- src/Bullet.java
+++ src/Bullet.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Bullet {
     protected int[][] bullet_map = new int[99][99];
     public Bullet() {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
+        for (int i = 0; i < 99; i++) {
+            for (int j = 0; j < 99; j++) {
                 bullet_map[i][j] = 0;
             }
         }
@@ -16,11 +16,11 @@ public class Bullet {
             if(i == 0){
                 bullet_map[new_locate][0] = 1;
             }else if(i == 1){
-                bullet_map[new_locate][99] = 2;
+                bullet_map[new_locate][98] = 2;
             }else if(i == 2){
                 bullet_map[0][new_locate] = 3;
             }else if(i == 3){
-                bullet_map[99][new_locate] = 4;
+                bullet_map[98][new_locate] = 4;
             }
         }
     }
