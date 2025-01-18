1699f45b0c35ae3f476bf395e0293f3ad4487d65
polarfarmer <a01040798163@gmail.com>
2025-01-18 16:03:17 +0900

    move bullet

--- src/Bullet.java
+++ src/Bullet.java
@@ -8,19 +8,19 @@ public class Bullet {
     private void New_Bullet() {
         Random random = new Random ();
         int i = random.nextInt(4);
-        if(i == 0){ //위에서 내려오는놈
+        if(i == 0){ //v
             x = random.nextInt(98);
             y = 0;
             dir = 1;
-        }else if(i == 1){
+        }else if(i == 1){ //^
             x = random.nextInt(98);
             y = 98;
             dir = 2;
-        }else if(i == 2){
+        }else if(i == 2){//>
             x = 0;
             y = random.nextInt(98);
             dir = 3;
-        }else if(i == 3){
+        }else if(i == 3){//<
             x = 98;
             y = random.nextInt(98);
             dir = 4;
@@ -28,6 +28,17 @@ public class Bullet {
     }
 
     private void Move_Bullet() {
-
+        if(dir==1) {
+            y++;
+        }
+        if(dir==2) {
+            y--;
+        }
+        if(dir==3) {
+            x++;
+        }
+        if(dir==4) {
+            x--;
+        }
     }
 }
